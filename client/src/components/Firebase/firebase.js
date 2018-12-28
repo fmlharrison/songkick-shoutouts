e79781967403432a0c13fa5b721f2d34f5b6a905
Felix Harrison <felix.harrison@songkick.com>
2018-12-28 19:58:03 +0000

    Add authentication api to app

--- client/src/components/Firebase/firebase.js
+++ client/src/components/Firebase/firebase.js
@@ -1,4 +1,5 @@
 import app from "firebase/app";
+import "firebase/auth";
 import "firebase/database";
 import Config from "./serviceAccountKey";
 
@@ -12,11 +13,24 @@ class Firebase {
     app.initializeApp(firebaseConfig);
 
     this.database = app.database();
+    this.auth = app.auth();
   }
 
   databaseReference = () => this.database.ref("shoutouts");
 
   databaseTimeStamp = () => app.database.ServerValue.TIMESTAMP;
+
+  doCreateUserWithEmailAndPassword = (email, password) =>
+    this.auth.createUserWithEmailAndPassword(email, password);
+
+  doSignInWithEmailAndPassword = (email, password) =>
+    this.auth.signInWithEmailAndPassword(email, password);
+
+  doSignOut = () => this.auth.signOut();
+
+  doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+
+  doUpdatePassword = password => this.auth.currentUser.updatePassword(password);
 }
 
 export default Firebase;
